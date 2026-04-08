f792acc9b99c0e27aa7e5113bfc6408a3a98293a
AlexandraDigital <futuresuccess105@gmail.com>
2026-04-08 09:05:26 -0400

    Update and rename Assets/  Scenes/ Scripts/rrr to Assets/  Scripts/PlayerController.cs

---
+++ Assets/  Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    float xRotation = 0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouseX);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.parent.right * x + transform.parent.forward * z;
+        transform.parent.position += move * speed * Time.deltaTime;
+    }
+}
